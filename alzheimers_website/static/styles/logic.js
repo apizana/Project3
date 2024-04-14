2ef8b8f158d6ab29dde677d2e5b714c3438c6bec
Amanda Garza <143903500+ajgarza44@users.noreply.github.com>
2024-04-13 21:40:53 -0500

    commit

--- alzheimers_website/static/styles/logic.js
+++ alzheimers_website/static/styles/logic.js
@@ -11,7 +11,7 @@ var myMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 });
 
 // base layer
-var myMap = L.map("map", {
+let myMap = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5,
   layers:[myMap]
@@ -23,7 +23,7 @@ var baseMaps ={
 };
 myMap
 // An array containing each city's name, location, and population
-let map = [{
+let cities = [{
   location: [],
   name: "Abilene",
   population: 8550405
