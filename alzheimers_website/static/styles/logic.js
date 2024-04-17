9bed20bdecf8d4dd7d839097bb7bfa89422a530f
Amanda Garza <143903500+ajgarza44@users.noreply.github.com>
2024-04-17 10:24:30 -0500

    commit

--- alzheimers_website/static/styles/logic.js
+++ alzheimers_website/static/styles/logic.js
@@ -8,7 +8,49 @@ xmlhttp.onreadystatechange = function(){
         console.log(data)
     }
   }
+// initialize function to pull ID numbers
+function init() {
+  var dropDown = d3.select("#selDataset");
+  // retrieve JSON data
+  d3.json(url).then(function (data) {
+    var sampleId = data.names;
+    sampleId.forEach((sample) => {
+        dropDown.append("option").text(sample).property("value", sample)
+    });
+    var initSample = sampleId[0];
+    buildDemo(initSample);
+    buildCharts(initSample);
+});
+};
 
+  // build function to create charts 
+function buildCharts(sample) {
+  d3.json(url).then(function (data) {
+      // variables for charts
+      var allSamples = data.samples;
+      var sampleInfo = allSamples.filter(row => row.id == sample);
+      var sampleValues = sampleInfo[0].sample_values;
+      var sampleValuesSlice = sampleValues.slice(0,10).reverse();
+      var otuIds = sampleInfo[0].otu_ids;
+      var otuIdsSlice = otuIds.slice(0,10).reverse();
+      var otuLabels = sampleInfo[0].otu_labels;
+      var otuLabelsSlice = otuLabels.slice(0,10).reverse();
+      var metaData = data.metadata;
+      var metaDataSample = metaData.filter(row => row.id == sample);
+      var wash = metaDataSample[0].wfreq;
+
+      // build chart 1
+      var trace1 = {
+          x: sampleValuesSlice,
+          y: otuIdsSlice.map(item => `OTU ${item}`),
+          type: "bar",
+          orientation: "h",
+          text: otuLabelsSlice,
+      };
+      var data = [trace1];
+      Plotly.newPlot("bar", data)
+  });
+};
 
 let myMap = L.map("map", {
   center: [37.09, -95.71],
@@ -320,4 +362,5 @@ for (let i = 0; i < cities.length; i++) {
     .addTo(myMap);
 } 
 
+init();
 
--- test.js
+++ test.js
@@ -12,7 +12,49 @@ xmlhttp.onreadystatechange = function(){
     }
 
 }
+// initialize function to pull ID numbers
+function init() {
+    var dropDown = d3.select("#selDataset");
+    // retrieve JSON data
+    d3.json(url).then(function (data) {
+        var sampleId = data.names;
+        sampleId.forEach((sample) => {
+            dropDown.append("option").text(sample).property("value", sample)
+        });
+        var initSample = sampleId[0];
+        buildDemo(initSample);
+        buildCharts(initSample);
+    });
+};
 
+// build function to create charts 
+function buildCharts(sample) {
+    d3.json(url).then(function (data) {
+        // variables for charts
+        var allSamples = data.samples;
+        var sampleInfo = allSamples.filter(row => row.id == sample);
+        var sampleValues = sampleInfo[0].sample_values;
+        var sampleValuesSlice = sampleValues.slice(0,10).reverse();
+        var otuIds = sampleInfo[0].otu_ids;
+        var otuIdsSlice = otuIds.slice(0,10).reverse();
+        var otuLabels = sampleInfo[0].otu_labels;
+        var otuLabelsSlice = otuLabels.slice(0,10).reverse();
+        var metaData = data.metadata;
+        var metaDataSample = metaData.filter(row => row.id == sample);
+        var wash = metaDataSample[0].wfreq;
+
+        // build chart 1
+        var trace1 = {
+            x: sampleValuesSlice,
+            y: otuIdsSlice.map(item => `OTU ${item}`),
+            type: "bar",
+            orientation: "h",
+            text: otuLabelsSlice,
+        };
+        var data = [trace1];
+        Plotly.newPlot("bar", data)
+    });
+};    
 //This is JsonData.json
 //{"date_population": [
 // 		{
