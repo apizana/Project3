b2b63c42086834ecab70fb48ca5cdb97ecf27876
Amanda Garza <143903500+ajgarza44@users.noreply.github.com>
2024-04-17 10:37:17 -0500

    commit

--- test.js
+++ test.js
@@ -6,66 +6,40 @@ xmlhttp.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
         var data = JSON.parse(this.responseText);
         console.log(data)
-
-
-
     }
-
-}
+  }
 // initialize function to pull ID numbers
 function init() {
-    var dropDown = d3.select("#selDataset");
-    // retrieve JSON data
-    d3.json(url).then(function (data) {
-        var sampleId = data.names;
-        sampleId.forEach((sample) => {
-            dropDown.append("option").text(sample).property("value", sample)
-        });
-        var initSample = sampleId[0];
-        buildDemo(initSample);
-        buildCharts(initSample);
+  var dropDown = d3.select("#selDataset");
+  // retrieve JSON data
+  d3.json(url).then(function (data) {
+    var sampleId = data.names;
+    sampleId.forEach((sample) => {
+        dropDown.append("option").text(sample).property("value", sample)
     });
+    var initSample = sampleId[0];
+    buildDemo(initSample);
+    buildCharts(initSample);
+});
 };
 
-// build function to create charts 
-function buildCharts(sample) {
-    d3.json(url).then(function (data) {
-        // variables for charts
-        var allSamples = data.samples;
-        var sampleInfo = allSamples.filter(row => row.id == sample);
-        var sampleValues = sampleInfo[0].sample_values;
-        var sampleValuesSlice = sampleValues.slice(0,10).reverse();
-        var otuIds = sampleInfo[0].otu_ids;
-        var otuIdsSlice = otuIds.slice(0,10).reverse();
-        var otuLabels = sampleInfo[0].otu_labels;
-        var otuLabelsSlice = otuLabels.slice(0,10).reverse();
-        var metaData = data.metadata;
-        var metaDataSample = metaData.filter(row => row.id == sample);
-        var wash = metaDataSample[0].wfreq;
+//build demographic info 
+function buildDemo(sample) {
+  var demo = d3.select("#selDataset");
+  d3.json(url).then(function (data) {
+      var metaData = data.metadata;
+      var metaDataSample = metaData.filter(row => row.id == sample);
+      demo.selectAll("p").remove();
+      metaDataSample.forEach((row) => {
+          for (const [key, value] of Object.entries(row)) {
+              demo.append("p").text(`${key}: ${value}`);
+          };
+      });
+  });
+};
 
-        // build chart 1
-        var trace1 = {
-            x: sampleValuesSlice,
-            y: otuIdsSlice.map(item => `OTU ${item}`),
-            type: "bar",
-            orientation: "h",
-            text: otuLabelsSlice,
-        };
-        var data = [trace1];
-        Plotly.newPlot("bar", data)
-    });
-};    
-//This is JsonData.json
-//{"date_population": [
-// 		{
-// 		  "date": "1941",
-// 		  "population": 406760
-// 		},{
-// 		  "date": "1951",
-// 		  "population": 778977
-// 		},{
-// 		  "date": "1961",
-// 		  "population": 1207000
-// 		}
-// 	]
-// }
+// 
+function optionChanged(sample) {
+  buildDemo(sample);
+  buildCharts(sample);
+};
